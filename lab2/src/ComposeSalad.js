2fae9e07c50aece107aed49a10815495b7b8efe9
William Hansson <wi3542ha-s@student.lu.se>
2023-09-12 14:23:13 +0200

    sas

--- lab2/src/ComposeSalad.js
+++ lab2/src/ComposeSalad.js
@@ -10,27 +10,36 @@ function ComposeSalad(props) {
     <div className="continer col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
 
-        
+
         <h2>Välj bas</h2>
         <div class="dropdown">
-        <select name="bas" id="bas-select">
-          <option value="">--Please choose an option--</option>
-        </select>
+          <select name="bas" id="bas-select">
+            <option value="">--Please choose an option--</option>
+          </select>
         </div>
         {foundations.map(name => <div key={name} className="col-4">{name}</div>)}
-        
+
         <h2>Välj protein</h2>
         <div class="dropdown">
-        <select name="protein" id="protein-select">
-          <option value="">--Please choose an option--</option>
-        </select>
+          <select name="protein" id="protein-select">
+            <option value="">--Please choose an option--</option>
+          </select>
         </div>
 
         <h2>Välj innehållet i din sallad</h2>
-        
-        {extras.map(name => <div key={name} className="col-4"> <input type="checkbox" id={name} name={name} /> {name} 
-        
+
+        {extras.map(name => <div key={name} className="col-4"> <input type="checkbox" id={name} name={name} /> {name}
+
         </div>)}
+
+        <h2>Välj en dressing</h2>
+        <div class="dropdown">
+          <select name="dressing" id="dressing-select">
+            <option value="">--Please choose an option--</option>
+          </select>
+        </div>
+        <div><button type="button">Lägg beställning!</button></div>
+        
 
       </div>
     </div>
